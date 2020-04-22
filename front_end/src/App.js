94eb8979d28a7ab24356b2ee6dc3e39a3e138c6d
Badr4u <badrache@hotmail.fr>
2020-04-22 23:07:15 +0000

    22/04/2020 front_end changes
    -Add React Router
    -Add Material ui
    -Add Material icons
    -Creation of (Landing /Signup / signIn Conponements)

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import SignUp from "./conponements/Layouts/SignUp"
+import SignIn from "./conponements/Layouts/SignIn"
+import MainPage from './conponements/Layouts/MainPage'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+            <Route path="/SignIn" component={SignIn} />
+            <Route path="/SignUp" component={SignUp} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ front_end/src/conponements/Layouts/MainPage.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import NavBar from '../NavBar'
+export default class MainPage extends Component {
+    render() {
+        return (
+            <div>
+                <NavBar />
+                <h1>Main Page</h1>
+            </div>
+        )
+    }
+}
