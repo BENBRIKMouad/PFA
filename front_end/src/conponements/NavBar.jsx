cf3a410f550f0d74a6682f478af297dcff7b67f9
Badr4u <badrache@hotmail.fr>
2020-04-23 15:00:16 +0000

    removing extra
    removing node_modules and putting it into fon't_end

--- front_end/src/conponements/NavBar.jsx
+++ front_end/src/conponements/NavBar.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 const NavBar = () => {
     return (
         <div>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon />
                     </IconButton>
                     <Typography variant="h6" >
                         News
