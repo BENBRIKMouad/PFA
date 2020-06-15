6612781fbc5c512d8d3f9916cdf1a68e3eb69edb
Badr4u <badrache@hotmail.fr>
2020-06-15 18:02:15 +0100

    Inmproving NavBar

--- front_end/src/Componenets/NavBarComp/NavBarComp.jsx
+++ front_end/src/Componenets/NavBarComp/NavBarComp.jsx
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Dropdown, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../../Store/Actions/auth";
+import axios from "axios";
 
 export class NavBarComp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Info: {},
+      loading: true,
+    };
+  }
+
+  ApiCall = () => {
+    const token = localStorage.getItem("token");
+    axios
+      .post("http://127.0.0.1:8000/api/TokenView/", {
+        token: token,
+      })
+      .then(({ data }) => this.setState({ Info: data, loading: false }))
+      .catch((err) => console.log(err));
+  };
   render() {
+    console.log(this.state);
     return (
       <Navbar bg="light" expand="lg">
         <Link className="navbar-brand" to="/">
@@ -15,13 +35,35 @@ export class NavBarComp extends Component {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             {this.props.isAuthenticated ? (
-              <button
-                className="btn btn-outline-secondary mx-2"
-                onClick={this.props.logout}
-              >
-                {" "}
-                déconnecter
-              </button>
+              <>
+                {this.state.loading ? (
+                  this.ApiCall()
+                ) : (
+                  <>
+                    <NavDropdown title="Dropdown" id="nav-dropdown">
+                      {this.state.Info.is_admin === "True" ? (
+                        <NavDropdown.Item eventKey="4.1">
+                          <Link to="/adminv2">Admin Pannel</Link>
+                        </NavDropdown.Item>
+                      ) : (
+                        <NavDropdown.Item eventKey="4.1">
+                          <Link to="/adminv2">Client Pannel</Link>
+                        </NavDropdown.Item>
+                      )}
+
+                      <NavDropdown.Item eventKey="4.2">
+                        <Link>Settings</Link>
+                      </NavDropdown.Item>
+                    </NavDropdown>
+                  </>
+                )}
+                <button
+                  className="btn btn-outline-secondary mx-2"
+                  onClick={this.props.logout}
+                >
+                  déconnecter
+                </button>
+              </>
             ) : (
               <>
                 <Nav.Link>A propos</Nav.Link>
