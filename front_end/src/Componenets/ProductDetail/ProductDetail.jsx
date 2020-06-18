284458e51bad564233e2bc58ed14c849acd7a67c
Badr4u <badrache@hotmail.fr>
2020-06-18 22:06:09 +0100

    minor product detail

--- front_end/src/Componenets/ProductDetail/ProductDetail.jsx
+++ front_end/src/Componenets/ProductDetail/ProductDetail.jsx
@@ -10,19 +10,20 @@ class ProductDetail extends Component {
     this.state = {
       id: this.props.match.params.ID,
       data: {},
+      isFetched: false,
     };
     this.AddItem = this.AddItem.bind(this);
   }
   componentDidMount() {
     axios
-      .get(`http://127.0.0.1:8000/api/product/${this.state.id}`)
-      .then(({ data }) => this.setState({ data: data }))
+      .get(`http://127.0.0.1:8000/api/ProductView/${this.state.id}`)
+      .then(({ data }) => this.setState({ data: data, isFetched: true }))
       .catch((err) => console.log(err));
   }
 
   AddItem() {
     axios
-      .post(`http://127.0.0.1:8000/api/add_to_cart/`, { pk: 1 })
+      .post(`http://127.0.0.1:8000/api/add_to_cart/`, { pk: 1, id: 29 })
       .then((res) => console.log(res.data))
       .catch((err) => console.log(err));
   }
@@ -36,48 +37,56 @@ class ProductDetail extends Component {
         <br />
 
         <Container>
-          <h1 className="my-4">
-            {this.state.data.title}
-            <br />
-            {/* this.state.data.discount_price > 0 */}
-            {false ? (
-              <small className="text-danger">Ce Produit est en Promo</small>
-            ) : null}
-          </h1>
+          {this.state.isFetched ? (
+            <>
+              <h1 className="my-4">
+                {this.state.data.title}
+                <br />
+                {/* this.state.data.discount_price > 0 */}
+                {false ? (
+                  <small className="text-danger">Ce Produit est en Promo</small>
+                ) : null}
+              </h1>
 
-          <div className="row">
-            <div className="col-md-8">
-              <img className="img-fluid" src={this.state.data.photo} alt="" />
-            </div>
+              <div className="row">
+                <div className="col-md-8">
+                  <img
+                    className="img-fluid"
+                    src={this.state.data.photo}
+                    alt=""
+                  />
+                </div>
 
-            <div className="col-md-4">
-              <h3 className="my-3">Description :</h3>
-              <p>{this.state.data.description}</p>
-              <h3 className="my-3">Suppléments</h3>
-              <ul>
-                <li>Lorem Ipsum</li>
-              </ul>
-              <h3 className="my-3">Prix</h3>
-            </div>
-          </div>
-          {this.props.isAuthenticated ? (
-            <Button variant="primary" onClick={this.AddItem}>
-              Ajouter au panier
-            </Button>
-          ) : (
-            <Link to="/SignIn" replace>
-              {" "}
-              Connecte Toi
-            </Link>
-          )}
-          {/*  <img
-            src={this.state.data.photo}
-            alt={this.state.data.slug}
-            class="img-thumbnail float-left "
-          ></img>
-          <span className="display-3"> Description :</span>
-          <p className="lead">{this.state.data.description}</p>
-           */}
+                <div className="col-md-4">
+                  <h3 className="my-3">Description :</h3>
+                  <p>{this.state.data.description}</p>
+                  <h3 className="my-3">Suppléments</h3>
+                  <ul>
+                    {this.state.data.additional_items.length > 0 ? (
+                      this.state.data.additional_items.map((item) => (
+                        <li>
+                          {item.title} : {item.price} DH
+                        </li>
+                      ))
+                    ) : (
+                      <li>Rien</li>
+                    )}
+                  </ul>
+                  <h3 className="my-3">Prix</h3>
+                </div>
+              </div>
+              {this.props.isAuthenticated ? (
+                <Button variant="primary" onClick={this.AddItem}>
+                  Ajouter au panier
+                </Button>
+              ) : (
+                <Link to="/SignIn" replace>
+                  {" "}
+                  Connecte Toi
+                </Link>
+              )}
+            </>
+          ) : null}
         </Container>
       </React.Fragment>
     );
